bf0f460b19c753c73518c2445343d521cdf7ec32
Michael Schmidt <michael.schmidt@crea.asia>
2021-07-30 00:18:31 +0700

    added main method and unhappy case

--- day_calculator.py
+++ day_calculator.py
@@ -5,7 +5,7 @@ from datetime import date
 def main():
     # grab the first 2 input parameters and split them
     # e.g. python3 python-day-calculator.py 2/6/1983 22/6/1983
-    
+
     in_date = sys.argv[1]
     out_date = sys.argv[2]
     print(' There are ' + str(calculate_diff(in_date, out_date)) + ' day between ' + str(in_date) + ' and ' + str(out_date) )
@@ -18,6 +18,8 @@ def calculate_diff(in_date, out_date):
     start = date(start_input[2], start_input[1], start_input[0])
     end = date(end_input[2], end_input[1], end_input[0])
     diff = end - start
+
+    #substract one day to give the days in between.
     return str(diff.days - 1 )
 
 
--- test_day_calculator.py
+++ test_day_calculator.py
@@ -3,6 +3,11 @@ import unittest
 
 class day_calculator(unittest.TestCase):
     def test_date_diff(self):
+        self.assertEqual(calculate_diff('01/01/2001', '03/01/2001'),"1")
         self.assertEqual(calculate_diff('2/6/1983', '22/6/1983'),"19")
         self.assertEqual(calculate_diff('4/7/1984', '25/12/1984'),"173")
-        #self.assertEqual(calculate_diff('3/1/1989', '3/8/1983'),"2036")
+        self.assertEqual(calculate_diff('3/1/1989', '3/8/1983'),"2036") # should be 1980, should we support backwards counting?
+
+
+if __name__ == '__main__':
+    unittest.main()
