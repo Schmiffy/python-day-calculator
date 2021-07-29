c1c9390024d820c866db456892a6008f5cacdbdd
Michael Schmidt <michael.schmidt@crea.asia>
2021-07-29 23:43:38 +0700

    added all test cases

--- test_day_calculator.py
+++ test_day_calculator.py
@@ -3,4 +3,6 @@ import unittest
 
 class day_calculator(unittest.TestCase):
     def test_date_diff(self):
-        self.assertAlmostEqual(calculate_diff("2/6/1983", "22/6/1983"),20)
+        self.assertEqual(calculate_diff('2/6/1983', '22/6/1983'),"19")
+        self.assertEqual(calculate_diff('4/7/1984', '25/12/1984'),"173")
+        #self.assertEqual(calculate_diff('3/1/1989', '3/8/1983'),"2036")
