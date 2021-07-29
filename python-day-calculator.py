af685dbd5a4d2bf38d9b0513e95b92b1b58d8a83
Michael Schmidt <michael.schmidt@crea.asia>
2021-07-29 19:54:03 +0700

    added input as parameter

--- python-day-calculator.py
+++ python-day-calculator.py
@@ -1,9 +1,15 @@
+import sys
 from datetime import date
 
 
-start = date(2001, 1, 1)
+# grab the first 2 input parameters and split them
+# e.g. python3 python-day-calculator.py 2/6/1983 22/6/1983
+start_input = sys.argv[1].split('/')
+end_input = sys.argv[2].split('/')
 
-end = date(2001, 1, 3)
+
+start = date(int(start_input[2]), int(start_input[1]), int(start_input[0]))
+end = date(int(end_input[2]), int(end_input[1]), int(end_input[0]))
 
 difference = end - start
 
