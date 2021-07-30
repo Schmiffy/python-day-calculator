a3f3dcaeca1555ef60a4180fcbd43ff72a9aeca0
Michael Schmidt <michael.schmidt@crea.asia>
2021-07-30 19:28:58 +0700

    comment

--- test_day_calculator.py
+++ test_day_calculator.py
@@ -6,8 +6,7 @@ class day_calculator(unittest.TestCase):
         self.assertEqual(calculate_diff('01/01/2001', '03/01/2001'),"1")
         self.assertEqual(calculate_diff('2/6/1983', '22/6/1983'),"19")
         self.assertEqual(calculate_diff('4/7/1984', '25/12/1984'),"173")
-        self.assertEqual(calculate_diff('3/1/1989', '3/8/1983'),"2036") # should be 1980, should we support backwards counting?
-
+        self.assertEqual(calculate_diff('3/1/1989', '3/8/1983'),"2036") # should be 1980, should we support backwards counting? If so we also need to remove the -1
 
 if __name__ == '__main__':
     unittest.main()
