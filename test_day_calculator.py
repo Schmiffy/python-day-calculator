229aca4ce956cc684d58a95f89315e98e7d624a7
Michael Schmidt <michael.schmidt@crea.asia>
2021-08-01 16:05:36 +0700

    added a few more tests

--- test_day_calculator.py
+++ test_day_calculator.py
@@ -3,18 +3,21 @@ import unittest
 
 class day_calculator(unittest.TestCase):
     def test_date_diff(self):
-        self.assertEqual(calculate_diff('01/01/2001', '03/01/2001'),"1")
-        self.assertEqual(calculate_diff('2/6/1983', '22/6/1983'),"19")
-        self.assertEqual(calculate_diff('4/7/1984', '25/12/1984'),"173")
-        self.assertEqual(calculate_diff('3/1/1989', '3/8/1983'),"2036") # should be 1980, should we support backwards counting? If so we also need to remove the -1
+        self.assertEqual(calculate_diff(validate_date('01/01/2001'), validate_date('03/01/2001')),1)
+        self.assertEqual(calculate_diff(validate_date('2/6/1983'), validate_date('22/6/1983')),19)
+        self.assertEqual(calculate_diff(validate_date('4/7/1984'), validate_date('25/12/1984')),173)
+#        self.assertEqual(calculate_diff('3/1/1989', '3/8/1983'),"2036") # should be 1980, should we support backwards counting? If so we also need to remove the -1
 
 class date_validation(unittest.TestCase):
     def test_validate_date(self):
-        self.assertEqual(validate_date('01/01/2001'),'01/01/2001')
+        self.assertAlmostEqual(validate_date('01/01/1900'),datetime.strptime('01/01/1900', '%d/%m/%Y'))
+        self.assertAlmostEqual(validate_date('31/12/2999'),datetime.strptime('31/12/2999', '%d/%m/%Y'))
+        self.assertAlmostEqual(validate_date('01/01/2001'),datetime.strptime('01/01/2001', '%d/%m/%Y'))
+        self.assertAlmostEqual(validate_date('1/1/2001'),datetime.strptime('1/1/2001', '%d/%m/%Y'))
         self.assertRaises(Exception, validate_date,'01/33/2001')
+        self.assertRaises(Exception, validate_date,'2001/06/15')
+        self.assertRaises(Exception, validate_date,'2001/15/06')
         self.assertRaises(Exception, validate_date,"01/01/20xx")
-
-
 
 if __name__ == '__main__':
     unittest.main()
